9929d3f920a03c0ae2199030fe1f1d467e803a36
Hoang Khanh <khanhhd@KhanhHD.local>
2016-10-27 00:27:41 +0900

    logger redux middleware

--- src/index.js
+++ src/index.js
@@ -1,5 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from '../components/app';
+import { applyMiddleware, createStore } from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+const reducer = function (initialState = 0, action) {
+  if (action.type == "INC") {
+    return initialState + 1;
+  } else if (action.type == "DEC") {
+    return initialState - 1;
+  }
+  return initialState;
+}
+
+const logger =(store) => (next) => (action) => {
+  console.log("action", action);
+  action.type = "INC";
+  next(action);
+}
+
+
+const middleware = applyMiddleware(logger);
+
+
+const store = createStore(reducer, 1, middleware)
+
+store.subscribe(() => {
+  console.log("Store changed:", store.getState());
+})
+
+store.dispatch({type: "INC"})
+store.dispatch({type: "DEC"})
+store.dispatch({type: "DEC"})
